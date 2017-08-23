9582c0519896e5c5dc19033045eeed79d8153cb0
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-08-23 14:34:58 -0500

    Proyect Creation

---
+++ MVVM3/MVVM3/Pages/MasterPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM3.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            App.Navigator = Navigator;//esto es para definir ovbjetos globale en toda la aplicacions
+        }
+    }
+}
