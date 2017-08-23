85a23f0d167e4c6aa21535e0d2f42930b02fcc18
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-08-23 17:43:31 -0500

    video 85 menuItems con paginas creadas

---
+++ MVVM3/MVVM3/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,19 @@
+﻿using MVVM3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM3.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; }
+
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+    }
+}
---
+++ MVVM3/MVVM3/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM3.ViewModels
+{
+    public class MainViewModel
+    {
+        //sera para que pueda observar cada cosa del menuItenViewModel
+        public ObservableCollection<MenuItemViewModel> Menu{ get; set; }
+
+        public MainViewModel()
+        {
+            Menu =new ObservableCollection<MenuItemViewModel>();
+            LoadMenu();
+        }
+
+        private void LoadMenu()
+        {
+            Menu.Add(new MenuItemViewModel {
+                Icon="ic_action_orders.png",
+                PageName="MainPage",
+                Title="Pedidos"
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_customers.png",
+                PageName = "ClientsPage",
+                Title = "Clientes"
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_alarms.png",
+                PageName = "AlarmsPage",
+                Title = "Alarmas"
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_settings.png",
+                PageName = "SettingsPage",
+                Title = "Ajustes"
+            });
+
+
+        }
+    }
+}
