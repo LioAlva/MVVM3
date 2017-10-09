0ac913a4300d6bdbb6687c8c3e2245a8f7d87e19
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-10-08 20:42:24 -0500

    JQUERY MOD

---
+++ WebJquery/Models/js/script1.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('#inicioLink').click(function () {
+        $('#container').load('Partials/_inicio.html');
+    });
+
+    $('#acercaLink').click(function () {
+        $('#container').load('Partials/_acerca.html');
+    });
+
+    $('#contactoLink').click(function () {
+        $('#container').load('Partials/_acerca.html');
+    });
+
+
+    $('#inicioLink').click();//
+
+
+});
